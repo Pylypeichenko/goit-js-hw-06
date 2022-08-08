ce3e28e62ea4f871a5eef1a8e6895d305def1f49
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-08 16:04:02 +0300

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+// let items = [];
+
+// ingredients.forEach((ingredient) => {
+//   const ingredientItem = document.createElement("li");
+//   ingredientItem.textContent = `${ingredient}`;
+//   ingredientItem.classList.add("item");
+
+//   items.push(ingredientItem);
+// });
+
+const items = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = `${ingredient}`;
+  ingredientItem.classList.add("item");
+
+  return ingredientItem;
+});
+
+ingredientsList.append(...items);
