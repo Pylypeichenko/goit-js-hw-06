e3c312d1dd6f0845dc391c9a30c1224c13dc3ac9
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-22 14:38:51 +0300

    Update task-08.js

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+const email = loginForm.firstElementChild.firstElementChild;
+const password =
+  loginForm.firstElementChild.nextElementSibling.firstElementChild;
+const submitButton = loginForm.lastElementChild;
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (email.value === "" || password.value === "") {
+    window.alert("All fields should be filled !");
+  }
+
+  const data = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+
+  console.log(data);
+  loginForm.reset();
+});
