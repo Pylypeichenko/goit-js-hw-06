39ff801e2b1f27653597a1bedc6afea6945471f4
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-22 15:01:53 +0300

    Update task-09.js

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorButton = document.querySelector(".change-color");
+const colorHexValue = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", (event) => {
+  colorHexValue.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorHexValue.textContent;
+});
