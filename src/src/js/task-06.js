5733248bdad3fd8994174d50065d5bc70cfbffb4
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-23 18:56:19 +0300

    Update task-06.js

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -3,6 +3,7 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", (event) => {
   if (event.currentTarget.value.length !== Number(input.dataset.length)) {
     input.classList.add("invalid");
+    input.classList.remove("valid");
   } else {
     input.classList.remove("invalid");
     input.classList.add("valid");
